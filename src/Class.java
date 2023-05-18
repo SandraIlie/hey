762bfe6365363a5d768edf50dd3762c37ae27c81
SandraIlie <133694743+SandraIlie@users.noreply.github.com>
2023-05-18 17:55:58 +0200

    Add files via upload

---
+++ src/Class.java
@@ -0,0 +1,13 @@
+public class Class {
+
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+            sayHello("Michael");
+            sayGoodbye("Michael");
+        }
+        public static void sayHello (String name){System.out.println("Hello " + name );
+        }
+    //TODO: TEST
+
+    public static void sayGoodbye (String name) {System.out.println("Goodbye " + name);}
+}
